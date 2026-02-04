95d14584265eff8cbc67fb42757d672d69922361
Sebastian Vidal Aedo <sebastianvidalaedo@gmail.com>
2026-02-04 10:55:33 -0300

    Initial Commit

---
+++ NotifyPR/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  NotifyPR
+//
+//  Created by Sebastián Vidal Aedo on 04-02-26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ NotifyPR/NotifyPRApp.swift
@@ -0,0 +1,17 @@
+//
+//  NotifyPRApp.swift
+//  NotifyPR
+//
+//  Created by Sebastián Vidal Aedo on 04-02-26.
+//
+
+import SwiftUI
+
+@main
+struct NotifyPRApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
